38e951fe588866b95bbcda8d638f2928c8408911
Akanksha174 <akankshakumari4755@gmail.com>
2024-10-11 11:19:00 +0530

    Update Useref.js

--- src/Useref.js
+++ src/Useref.js
@@ -10,4 +10,23 @@ const resetInput=()=>{
     inputRef.current.value="";
     setName("")
 }
+    return(
+        <div>
+         <h2> UseRef Hook Example</h2>
+         <form onSubmit={handleSubmit}>
+            <input
+            ref={inputRef}
+            type='text'
+            placeholder='Enter your name'
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            >
+            </input>
+            <button type='Submit'>Submit</button>
+         </form>
+         <button onClick={resetInput}>Reset</button>
+
+
+        </div>       
+    )}
 export default App;
